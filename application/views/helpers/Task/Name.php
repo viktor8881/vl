b8fcab4528959da0458929ba8dabfa79002b8c3d
dev-victor <ivavictor@mail.ru>
2016-08-29 22:35:24 +0300

    fixed name task in list

--- application/views/helpers/Task/Name.php
+++ application/views/helpers/Task/Name.php
@@ -16,10 +16,17 @@ class View_Helper_Task_Name extends Zend_View_Helper_Abstract
     public function task_Name(Task_Model_Abstract $model) 
     {
         $xhtml = '<div class="row">';
+        if ($model->isModeOnlyUp()) {
+            $modeName = _('Рост');
+        }elseif($model->isModeOnlyDown()) {
+            $modeName = _('Понижение');
+        }else{
+            $modeName = _('Pост/понижение');
+        }
         if ($model->isPercent()) {
-            $xhtml .= '<div class="col-sm-12">'.sprintf(_('Изменение на %1$s за %2$s'), $this->view->formatPercent($model->getPercent(), true), $this->view->pluralDays($model->getPeriod(), true)).'</div>';
+            $xhtml .= '<div class="col-sm-12">'.sprintf(_('%1$s на %2$s за %3$s'), $modeName, $this->view->formatPercent($model->getPercent(), true), $this->view->pluralDays($model->getPeriod(), true)).'</div>';
         }elseif ($model->isOvertime()) {
-            $xhtml .= '<div class="col-sm-12">'.sprintf(_('Pост/падение в течении %s'), $this->view->PluralDaysGenitive($model->getPeriod(), true)).'</div>';
+            $xhtml .= '<div class="col-sm-12">'.sprintf(_('%1$s в течении %2$s'), $modeName, $this->view->PluralDaysGenitive($model->getPeriod(), true)).'</div>';
         }
         if ($model->countMetals()) {
             $xhtml .= '<div class="col-sm-3 text-success" style="padding-top:16px; padding-bottom:16px;">'
